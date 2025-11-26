189516bd9d3c3659e699fbd36e846aa3e968be8e
sanjaiveeran44 <kit27.am44@gmail.com>
2025-11-26 16:25:30 +0530

    files updated

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,12 @@
 import "./Hero.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Hero() {
+  const navigate = useNavigate();
+  
+  const handleGetStarted = () => {
+    navigate('/app');
+  }
   return (
     <section className="hero">
       <h1 className="hero-title">
@@ -8,9 +14,15 @@ export default function Hero() {
         FOR TEACHERS & STUDENTS
       </h1>
 
-      <button className="expand-btn">
+      <button className="expand-btn" onClick={handleGetStarted}>
         Get Started <span className="arrow">→</span>
       </button>
+
+        <p className="hero-text">
+          Create, present, and teach smarter — powered by AI.<br />
+          Turn slides into conversations, quizzes, and interactive learning.
+          Ask questions, get examples instantly, and test knowledge with AI-generated quizzes.
+        </p>
 
     </section>
   );
