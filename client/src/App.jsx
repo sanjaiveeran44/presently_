013ade3ee236da0cbae4ead0b4fac97905505d6d
sanjaiveeran44 <kit27.am44@gmail.com>
2025-11-26 16:27:07 +0530

    routes for homepage is difined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import LandingPage from './pages/landingPage';
+import HomePage from './pages/homePage';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
