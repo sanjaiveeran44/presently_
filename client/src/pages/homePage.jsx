3e149ea77aa1243c7f832518d18ed9825d506018
sanjaiveeran44 <kit27.am44@gmail.com>
2025-11-28 16:20:23 +0530

    landing page is updated with css

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,28 +1,56 @@
 import React, { useState } from 'react';
+import LeftMenu from '../components/LeftMenu';
+import CenterContainer from '../components/CenterContainer';
+import RightPanel from '../components/RightPanel';
 import './homePage.css';
-import Leftmenu from '../components/Leftmenu'
-import CenterContainer from '../components/CenterContainer'
-import RightPanel from '../components/RightPanel'
 
-export default function HomePage() {
 
-    const [isChatOpen , setIsChatOpen] = useState(false);
-    return (
-        <div className="Home">
-            <aside className="menu">
-             
-                <Leftmenu isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen}/>
-            </aside>
-            
-            <main className="center">
-                
-                <CenterContainer/>
-            </main>
-            
-            <aside className="right">
-               
-                {/* <RightPanel isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen}/> */}
-            </aside>
+const Home = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  return (
+    <div className="home-container">
+      <nav className="navbar">
+        <div className="navbar-left">
+          <button className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <div className="logo">
+            <span className="logo-p">P</span>
+            <span className="logo-text">resently</span>
+          </div>
         </div>
-    );
-}
+        <div className="nav-links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+        </div>
+      </nav>
+
+      <div className="main-content">
+        <LeftMenu 
+          isOpen={isMenuOpen} 
+          onClose={() => setIsMenuOpen(false)}
+          onOpenChat={() => setIsChatOpen(true)}
+        />
+        
+        <CenterContainer
+          isMenuOpen={isMenuOpen} 
+          isChatOpen={isChatOpen}
+        />
+        
+        <RightPanel 
+          isOpen={isChatOpen} 
+          onClose={() => setIsChatOpen(false)}
+        />
+      </div>
+      
+      
+    </div>
+  );
+};
+
+export default Home;
